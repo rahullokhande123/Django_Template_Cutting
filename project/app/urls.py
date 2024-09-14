07d355b36e6d4817086b20f3631b3aa5990a0b3e
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-14 10:28:02 +0530

    Template Cutting {base.html and home.html}

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app import views
 
 urlpatterns=[
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('', views.my_link, name='my_link')
 ]
